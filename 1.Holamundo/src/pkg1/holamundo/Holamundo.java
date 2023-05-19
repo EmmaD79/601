e6309892a8176ef8dbb00485411f8642df750c9f
EmmaD79 <perla_cooper14@hotmail.com>
2023-05-19 07:54:47 -0600

    Se agrega programa 1.Holamundo

---
+++ 1.Holamundo/src/pkg1/holamundo/Holamundo.java
@@ -0,0 +1,16 @@
+
+package pkg1.holamundo;
+
+/**
+ *
+ * @author Emmita
+ */
+public class Holamundo {
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+       
+}
+    
+}
